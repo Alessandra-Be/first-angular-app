f6da111a2545522aacc98886227a849302533f4d
Alessandra-Be <alessandra.altavilla@bollettaexpert.it>
2025-07-21 16:33:35 +0200

    modifica css e html user e app

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user',
+  standalone: true,
   imports: [],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class User {
+export class UserComponent {
 
 }
