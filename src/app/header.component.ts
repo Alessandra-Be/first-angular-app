6181ffc9791b3cb2f2a83ced3be7a43ba754e495
Alessandra-Be <alessandra.altavilla@bollettaexpert.it>
2025-07-21 16:02:02 +0200

    add logo and doing the header

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
     selector: 'app-header',
     standalone: true,
     templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 
 export class HeaderComponent {}
