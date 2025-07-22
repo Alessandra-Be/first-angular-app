0dc3c8d03da82175211856f7a0b793110cf4a72c
Alessandra-Be <alessandra.altavilla@bollettaexpert.it>
2025-07-22 12:56:20 +0200

    stato e input

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,4 @@
-import { Component } from '@angular/core';
-
-import { DUMMY_USERS } from '../dummy-users';
-
-const randonIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -11,5 +7,14 @@ const randonIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randonIndex]; 
+  @Input() avatar!: string;
+  @Input() name!: string;
+  get imagePath() {
+    return `/users/` + this.avatar;
+  }
+
+  onSelectUser  () {}
 }
+
+
+
