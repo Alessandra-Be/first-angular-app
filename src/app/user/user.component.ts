1a4eefd11c574c33e33bf28e7a3f924986f67524
Alessandra-Be <alessandra.altavilla@bollettaexpert.it>
2025-07-23 12:17:10 +0200

    for, if, ngfor, ngif

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,16 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+// type User = {
+//     id: string;
+//     name: string;
+//     avatar: string;
+//   }
+
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -7,11 +19,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) user!: {
-    id: string;
-    name: string;
-    avatar: string;
-  }
+  @Input({required: true}) user!: User;
   @Output() select = new EventEmitter<string>()
 
   get imagePath() {
