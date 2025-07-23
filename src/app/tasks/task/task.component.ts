b8b1e4f0297e57c660af7fbbdf6e37f8d6bb8055
Alessandra-Be <alessandra.altavilla@bollettaexpert.it>
2025-07-23 13:09:35 +0200

    gestione task dummies con ciclo for

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-task',
   imports: [],
-  templateUrl: './task.html',
-  styleUrl: './task.css'
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
 })
 export class Task {
 
